c389f4430eab1bf6bb54321d4ea58ebf387f3d98
Miguel Silva <gisele.silva@sptech.school>
2024-04-18 21:18:29 -0300

    Inserção de componentes dinamicos atraves dos dados recebidos

--- js/buscar.js
+++ js/buscar.js
@@ -1,5 +1,4 @@
 async function buscar() {
-    console.log("Passei por aqui");
 
     const resposta = await fetch("https://660f44ad356b87a55c510d25.mockapi.io/agendas");
 
@@ -9,10 +8,33 @@ async function buscar() {
 
     const cards = document.getElementById("cards_games");
 
-    cards.innerHTML = respostaDadosAgenda.map((itemAgenda)=>{
-        return "Teste MUITO tOOOOP "
-    });
+    cards.innerHTML = respostaDadosAgenda.map((itemAgenda) => {
+        return `
+        <div class="cardItem">
+        <div class="dataGame"> 
+            <img src="../imagens/calendar-solid.svg" alt=""> ${itemAgenda.dataJogo}
+        </div>
+        <img class="img-background" src="${itemAgenda.urlImagens}" alt="">
+        <div class="descricao">
+            <p> <strong>${itemAgenda.nome}</strong> </p>
+            <p> ${itemAgenda.descricao} </p>
+            <p><strong>Gamers: </strong></p>
+
+            <div class="gamers">
+            ${itemAgenda.gamers.map((gamerItem) => {
+            return `<div class="gamerItem">@${gamerItem}</div>`
+        }).join('')
+            }
+            </div>          
+        </div>  
+        <div class="assistir">
+                <a class="assistirItem"> <img src="../imagens/youtube.svg" alt=""> Assistir </a>
+        </div>
+    </div>
+    `
+
+    }).join('');
 }
-console.log("antes de buscar");
+
 buscar();
-console.log("depois de buscar");
+
